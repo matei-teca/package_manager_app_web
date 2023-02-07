a4131de8137968d1da3321f53291800664f9f5c1
Matei Teca <matei.teca@gmail.com>
2023-02-07 14:36:45 +0200

    project setup

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,5 +1,23 @@
-const loadEvent = _ => {
+let rootEl;
 
+const formStructure = () => {
+    return `
+    <form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+    </form>
+    `
+}
+
+const displayForm = () => {
+    rootEl = document.getElementById("root");
+    rootEl.insertAdjacentHTML("beforeend", formStructure());
+}
+
+const loadEvent = _ => {
+    displayForm();
 };
 
 window.addEventListener("load", loadEvent);
